8fa1b3e8d53fa9c6dd37bec2458da6be75c2e962
Fox Caulfield <mrcoderprog@gmail.com>
2021-03-28 15:05:10 +0300

    Add provider to provide store
    Add react-redux library to provide store

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Introduction from "./components/Introduction/Introduction";
 import Tutorial from "./components/Introduction/Tutorial/Tutorial";
 import Interface from "./components/Interface/Interface";
+import { Provider } from "react-redux";
+import store from "./store/reduxStore";
 
 function App() {
   return (
-    //   <div>
-    //   </div>
-    // );
     <div className="App">
-      {/* <p>The Battleship Game</p> */}
-      <Header />
-      <Introduction />
-      <Tutorial />
-      <Interface />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Header />
+        <Introduction />
+        <Tutorial />
+        <Interface />
+      </Provider>
     </div>
   );
 }
